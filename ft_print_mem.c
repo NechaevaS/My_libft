ab7052123e87bbe3f92552ad2643e775413096c7
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-03-05 14:42:37 -0800

    norm

--- ft_print_mem.c
+++ ft_print_mem.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_mem.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snechaev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/05 14:24:31 by snechaev          #+#    #+#             */
+/*   Updated: 2019/03/05 14:26:04 by snechaev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include "libft.h" 
+#include "libft.h"
 
-void ft_print_mem(unsigned char *str, size_t size)
+void	ft_print_mem(unsigned char *str, size_t size)
 {
-
 	while (size)
 	{
 		printf("%02x ", *str);
